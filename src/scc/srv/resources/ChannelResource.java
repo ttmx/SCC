7e37cfb62a1b4a29b8a4f9f09e8b9d62975c57eb
João Bordalo <j.bordalo@campus.fct.unl.pt>
2021-11-04 16:48:49 +0000

    Implement Channel deletion, member listing and member managing

--- src/scc/srv/resources/ChannelResource.java
+++ src/scc/srv/resources/ChannelResource.java
@@ -1,13 +1,17 @@
 package scc.srv.resources;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
 import scc.entities.Channel;
+import scc.entities.Message;
 import scc.srv.DataAbstractionLayer;
 import scc.utils.Log;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Path("/channel")
@@ -18,7 +22,6 @@ public class ChannelResource {
 
     public ChannelResource(DataAbstractionLayer data) {
         mCol = data.getChannelCol();
-        //TODO MCOL? UNNECESSARY?
         this.data = data;
     }
 
@@ -26,11 +29,21 @@ public class ChannelResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Channel getChannel(@PathParam("id") String id) {
-        Document channelDoc = mCol.find(new Document("_id", id)).first();
+        Document channelDoc = this.mCol.find(new Document("_id", id)).first();
         if (channelDoc != null) {
             return Channel.fromDocument(channelDoc);
         }
         throw new NotFoundException();
+    }
+
+    @Path("/{id}")
+    @DELETE
+    public void deleteChannel(@PathParam("id") String id) {
+        // TODO Authenticate, garbage collect users and messages
+
+        DeleteResult result = this.mCol.deleteOne(new Document("_id", id));
+
+        if (result.getDeletedCount() == 0) throw new NotFoundException();
     }
 
     @Path("/")
@@ -53,23 +66,61 @@ public class ChannelResource {
 
     @Path("/{id}/add/{userId}")
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
     public void addUserToChannel(@PathParam("id") String id, @PathParam("userId") String userId) {
-        Document channelDoc = mCol.find(new Document("_id", id)).first();
-
-        // TODO make update better with array operations
-        // TODO make sure user is in the channel to add?
+        Document channelDoc = this.mCol.find(new Document("_id", id)).first();
 
         if (channelDoc != null) {
-            mCol.updateOne(new Document("_id", id), new Document("$addToSet" , new Document("members", id)));
+            // Add user to channel
+            this.mCol.updateOne(new Document("_id", id), new Document("$addToSet" , new Document("members", userId)));
 
-            // TODO add channel to user's list
+            // Add channel to user
             data.getUserCol().updateOne(new Document("_id", userId), new Document("$addToSet" , new Document("channelIds", id)));
+        } else {
+            throw new BadRequestException();
         }
-        throw new BadRequestException();
+    }
+
+    @Path("/{id}/remove/{userId}")
+    @DELETE
+    public void removeUserFromChannel(@PathParam("id") String id, @PathParam("userId") String userId) {
+        Document channelDoc = this.mCol.find(new Document("_id", id)).first();
+
+        if (channelDoc != null) {
+            // Remove user from channel
+            this.mCol.updateOne(new Document("_id", id), new Document("$pull", new Document("members", userId)));
+
+            // Remove channel from user
+            data.getUserCol().updateOne(new Document("_id", userId), new Document("$pull" , new Document("channelIds", id)));
+        } else {
+            throw new BadRequestException();
+        }
+    }
+
+    @Path("/{id}/messages")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object[] getChannelMessages(@PathParam("id") String id) {
+        List<Document> messageDocs = this.data.getMessageCol().find(new Document("channel", id)).into(new ArrayList<>());
+
+        if (messageDocs != null) {
+            Object[] messages = messageDocs.stream().map(e -> Message.fromDocument(e)).toArray();
+            return messages;
+        }
+        throw new NotFoundException();
+    }
+
+    @Path("/{id}/users")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String[] getChannelMembers(@PathParam("id") String id) {
+        Document channelDoc = this.mCol.find(new Document("_id", id)).first();
+        if (channelDoc != null) {
+            return Channel.fromDocument(channelDoc).getMembers();
+        }
+        throw new NotFoundException();
     }
 
     private void insertChannel(Channel c) {
-        mCol.insertOne(c.toDocument());
+        this.mCol.insertOne(c.toDocument());
     }
 }
